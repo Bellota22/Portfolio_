a84e811d3a8865abb53df957e6bf59b1d1d03de8
Gabriel Villanueva <gvillanuevavega@gmail.com>
2024-01-04 22:18:10 -0500

    fixing padding navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div className='fixed top-0 left-0 right-0 h-[40px] bg-transparent backdrop-filter backdrop-blur-md z-50 border-b border-bg-header flex justify-between lg:h-[64px] '>
+    <div className='fixed top-0 left-0 right-0 h-[40px] bg-transparent backdrop-filter backdrop-blur-md z-50 border-b border-bg-header flex justify-between lg:h-[64px] px-4  '>
     
-      <div className='text-[26px] text-white flex items-end leading-none p-2 lg:text-[36px] ' >
+      <div className='text-[26px]  text-white flex items-end leading-none p-2 lg:text-[36px] ' >
         GabCode
         <div className='rounded-full bg-green-700/50 w-[10px] h-[10px] flex items-baseline '/>
       </div>
